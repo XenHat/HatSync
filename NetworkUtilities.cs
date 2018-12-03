a95a680754da33b0abf3242de8589a47d5dd2eeb
XenHat <XenHat@users.noreply.github.com>
2018-12-02 23:15:50 -0500

    NetworkUtilities also unused

--- NetworkUtilities.cs
+++ NetworkUtilities.cs
@@ -1,7 +1,7 @@
 ﻿// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
 
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
-
+/*
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -76,3 +76,4 @@ public static partial class NetworkUtilities
         return urlContent;
     }
 }
+*/
